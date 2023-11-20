39674ae8d19c0574d54fe66b44343ae0bc6a199a
jesusmaingCS <139652628+jesusmaingCS@users.noreply.github.com>
2023-11-20 15:09:55 -0700

    social net

--- src/components/FYFFooter.js
+++ src/components/FYFFooter.js
@@ -14,24 +14,23 @@ import  logo  from "../assets/img/logo-white.png";
 
 const FYFFooter = () => {
   return (
-    <div className="bg-black  py-20 font-titleFont ">
-      <div className="max-w-screen-xl flex items-center justify-center">
-        <div className="flex flex-col gap-7 ">
-          <img className="w-52" src={logo} alt="logoLight" />
-          <img className="w-56" src={paymentLogo} alt="paymentLogo" />
-          <div className="flex gap-5 text-lg text-gray-400">
-            <ImGithub className="hover:text-white duration-300 cursor-pointer" />
-            <FaYoutube className="hover:text-white duration-300 cursor-pointer" />
-            <FaFacebookF className="hover:text-white duration-300 cursor-pointer" />
-            <FaTwitter className="hover:text-white duration-300 cursor-pointer" />
-            <FaInstagram className="hover:text-white duration-300 cursor-pointer" />
-          </div>
+    <div className="bg-black py-20 font-titleFont">
+    <div className="max-w-screen-xl flex items-center justify-center">
+      <div className="flex flex-col gap-7">
+        <img className="w-52" src={logo} alt="logoLight" />
+        <img className="w-56" src={paymentLogo} alt="paymentLogo" />
+        <div className="flex gap-5 text-lg text-gray-400">
+          <a href="https://www.facebook.com/profile.php?id=61552378797588" target="_blank" rel="noopener noreferrer">
+            <FaFacebookF className="text-white duration-300 cursor-pointer"/>
+          </a>
+          <a href="https://www.instagram.com/fajasyfajasaz/?igshid=NzZlODBkYWE4Ng%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-white duration-300 cursor-pointer"/>
+          </a>
         </div>
-       
-       
-        
       </div>
     </div>
+  </div>
+  
   );
 };
 
